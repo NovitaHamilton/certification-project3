a17e697f0e9289899688839644dc172d32ca8182
Novita H <novitahamilton@gmail.com>
2024-03-06 12:29:26 -0800

    Add delete, edit, and close icons to IndividualTaskList component
    1. Install React Material UI to get icon components
    2. Add functionality to close 'x' icon. It will navigate back to /tasklists when it's clicked.

--- src/components/IndividualTaskList.jsx
+++ src/components/IndividualTaskList.jsx
@@ -1,20 +1,34 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import Task from './Task';
 import Button from './common/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import CloseIcon from '@mui/icons-material/Close';
 
 function IndividualTaskList({ tasklists }) {
-  const { id } = useParams();
+  const navigate = useNavigate();
+  const backToTaskLists = (e) => {
+    e.preventDefault();
+    navigate(`/tasklists`);
+  };
 
   const openAddTaskForm = () => {};
 
+  const { id } = useParams();
   // Find the task list with the specified ID
   const tasklist = tasklists.find((tasklist) => tasklist.id === id);
 
   return (
     <div className="individual-task">
-      <h1>{tasklist.name}</h1>
+      <CloseIcon onClick={backToTaskLists} />
+      <h2>{tasklist.name}</h2>
       <Button onCLick={openAddTaskForm}>Add Task</Button>
+      <div>
+        <DeleteIcon />
+        <EditIcon />
+      </div>
+
       <ul>
         {/* Render tasks for the individual task list*/}
         {tasklist.tasks.map((task) => (
